561931a6da86ccdb624930aa22255a2f0ba01caf
anp22 <agustin.navarro@pm.me>
2025-06-06 12:19:21 -0400

    Upgrade to version -.7.0

--- sanitazer13.pl
+++ sanitazer13.pl
@@ -7,9 +7,10 @@ use v5.38;
 use Text::Unidecode;
 use Date::Simple ( 'date', 'today' );
 use File::Basename;
+use Text::Capitalize;
 use warnings;
 use Cwd;
-our $VERSION = qv('0.6.0');
+our $VERSION = ('0.6.0');
 
 =pod
          ___  __ _ _ __ (_) |_ __ _ _______ _ __
@@ -29,13 +30,15 @@ our $VERSION = qv('0.6.0');
 
 =head1 VERSION
 
-our $VERSION = qv('0.6.0');
+our $VERSION = qv('0.7.0');
 
-   Date: Thu Dec  7 01:36:31 PM -04 2023
+   Date: Thu June  6 11:21:26 AM -04 2023
+
 
 =head1 CHANGES :
  
-  REF NO  VERSION DATE    WHO     DETAIL
+  REF NO  VERSION  DATE    WHO     DETAIL
+  001 01    070		260606	 anp		remplaze subroutine titlecase by Text::Capitalize from CPAN
 
 
 =head1 License
@@ -49,6 +52,7 @@ our $VERSION = qv('0.6.0');
     If not, see <https://www.gnu.org/licenses/>. 
  
 Copyright (C) 2025 Agustin Navarro (agustin.navarro@gmail.com)
+Copyright (C) 2026 Agustin Navarro (agustin.navarro@gmail.com)
 
 =cut
 
@@ -89,7 +93,7 @@ if ( length($ext) > 5 ) {
 
 $ofname = raccents("$fname");
 $ofname =~ s/[-|_]/\ /g;
-$ofname  = titlecase($ofname);
+$ofname  = capitalize_title($ofname);
 $ofname  = sanitaze("$ofname");
 $ofnamex = $ofname;
 if ( $ext ne "" ) {
@@ -115,94 +119,6 @@ sub raccents {
     $odir = unidecode($odir);
     $odir =~ s/\p{NonspacingMark}//g;
     return ($odir);
-}
-
-sub titlecase {
-
-    # 	This filter changes all words to Title Caps, and attempts to be clever
-    #	about *un*capitalizing small words like a/an/the in the input.
-    #
-    #	The list of "small words" which are not capped comes from
-    #	the New York Times Manual of Style, plus 'vs' and 'v'.
-    #
-    #	10 May 2008
-    #	Original version by John Gruber:
-    #	http://daringfireball.net/2008/05/title_case
-    #
-    #	28 July 2008
-    #	Re-written and much improved by Aristotle Pagaltzis:
-    #	http://plasmasturm.org/code/titlecase/
-    #
-    #   Full change log at __END__.
-    #
-    #	License: http://www.opensource.org/licenses/mit-license.php
-    # Changes:
-    # Thu, 06 Nov 2014
-    # - Removed /o switch from substitutions; it's out-dated and described now as only "pretending" to optimize
-    # - Special cases for small words in two-word compounds, like "stand0-in" and "in-flight" (but not "man-in-the-middle")
-    # Mon, 26 Oct 2020
-    # - Removed the while(<>) loop. now is a subroutine. agustin.navarro@gmail.com
-    #
-    my @small_words = qw( (?<!q&)a an and as at(?!&t) but by en for if in of on or the to v[.]? via vs[.]? );
-    my $small_re    = join '|', @small_words;
-    my $apos        = qr/ (?: ['’] [[:lower:]]* )? /x;
-    $_ = qq{@_};
-    s/\-/\ /g;
-    s{\A\s+}{};
-    s{\s+\z}{};
-    $_ = lc $_ if not /[[:lower:]]/;
-    s{
-        \b (_*) (?:
-            ( (?<=[ ][/\\]) [[:alpha:]]+ [-_[:alpha:]/\\]+ |   # file path or
-              [-_[:alpha:]]+ [@.:] [-_[:alpha:]@.:/]+ $apos )  # URL, domain, or email
-            |
-            ( (?i: $small_re ) $apos )                         # or small word (case-insensitive)
-            |
-            ( [[:alpha:]] [[:lower:]'’()\[\]{}]* $apos )       # or word w/o internal caps
-            |
-            ( [[:alpha:]] [[:alpha:]'’()\[\]{}]* $apos )       # or some other word
-        ) (_*) \b
-    }{
-        $1 . (
-          defined $2 ? $2         # preserve URL, domain, or email
-        : defined $3 ? "\L$3"     # lowercase small word
-        : defined $4 ? "\u\L$4"   # capitalize word w/o internal caps
-        : $5                      # preserve other kinds of word
-        ) . $6
-    }xeg;
-
-    # Exceptions for small words: capitalize at start and end of title
-    s{
-        (  \A [[:punct:]]*         # start of title...
-        |  [:.;?!][ ]+             # or of subsentence...
-        |  [ ]['"“‘(\[][ ]*     )  # or of inserted subphrase...
-        ( $small_re ) \b           # ... followed by small word
-    }{$1\u\L$2}xig;
-    s{
-        \b ( $small_re )      # small word...
-        (?= [[:punct:]]* \Z   # ... at the end of the title...
-        |   ['"’”)\]] [ ] )   # ... or of an inserted subphrase?
-    }{\u\L$1}xig;
-
-    # Exceptions for small words in hyphenated compound words
-    ## e.g. "in-flight" -> In-Flight
-    s{
-        \b
-        (?<! -)					# Negative lookbehind for a hyphen; we don't want to match man-in-the-middle but do want (in-flight)
-        ( $small_re )
-        (?= -[[:alpha:]]+)		# lookahead for "-someword"
-    }{\u\L$1}xig;
-    ## # e.g. "Stand-in" -> "Stand-In" (Stand is already capped at this point)
-    s{
-        \b
-        (?<!…)					# Negative lookbehind for a hyphen; we don't want to match man-in-the-middle but do want (stand-in)
-        ( [[:alpha:]]+- )		# $1 = first word and hyphen, should already be properly capped
-        ( $small_re )           # ... followed by small word
-        (?!	- )					# Negative lookahead for another '-'
-    }{$1\u$2}xig;
-
-    # print "%","$_ \n";
-    return ($_);
 }
 
 sub sanitaze {
